1838c472fc6cc1f78bfede1218c3d137fd61bea8
HongNT03 <nguyentienhong220903@gmail.com>
2024-12-06 15:43:52 +0700

    create sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import Dashprofile from "../components/Dashprofile";
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-54">
+        {/* side bar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === "profile" && <Dashprofile />}
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
