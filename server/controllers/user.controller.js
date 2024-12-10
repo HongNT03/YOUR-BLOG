ff6b2311c82702ce2a4d6a085b81378492c5dab7
HongNT03 <nguyentienhong220903@gmail.com>
2024-12-10 13:36:13 +0700

    create delete api

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -18,7 +18,9 @@ export const updateUser = async (req, res, next) => {
   }
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
-      return next(errorhandler(400, "Username must be between 7 and 20 characters"));
+      return next(
+        errorhandler(400, "Username must be between 7 and 20 characters")
+      );
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
       return next(errorhandler(400, "Username must be lowercase"));
@@ -44,6 +46,18 @@ export const updateUser = async (req, res, next) => {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorhandler(403, "You dont allow to delete this user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
   }
