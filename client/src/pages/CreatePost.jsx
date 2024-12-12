31f0a279ecfae17c912872cc80c869148c9d62a3
HongNT03 <nguyentienhong220903@gmail.com>
2024-12-12 16:35:51 +0700

    add upload post img

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, TextInput, FileInput, Button } from "flowbite-react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { uploadImage } from "/src/utils/cloudinary.js";
 
 const CreatePost = () => {
+  const [imageFile, setImageFile] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [data, setData] = useState(null);
+
+  const handleUpload = async () => {
+    setIsUploading(true);
+    try {
+      const result = await uploadImage(imageFile);
+      const imgUrlFromCloudinary = result.secure_url;
+      setData((prevData) => ({
+        ...prevData,
+        profilePicture: imgUrlFromCloudinary,
+      }));
+      console.log(imgUrlFromCloudinary);
+    } catch (error) {
+      console.error("Upload failed:", error);
+    } finally {
+      setIsUploading(false);
+    }
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+    }
+  };
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Create a Post</h1>
@@ -24,12 +53,17 @@ const CreatePost = () => {
           </Select>
         </div>
         <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-          <FileInput type="file" accept="image/*" />
+          <FileInput
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+          />
           <Button
             type="button"
             gradientDuoTone="purpleToBlue"
             size="sm"
             outline
+            onClick={handleUpload}
           >
             Upload Image
           </Button>
