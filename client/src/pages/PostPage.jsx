bbce408769430afdfc597b06e05b9444774983ce
HongNT03 <nguyentienhong220903@gmail.com>
2024-12-18 01:51:05 +0700

    add calltoaction

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
 import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 const PostPage = () => {
   const { postSlug } = useParams();
@@ -64,7 +65,13 @@ const PostPage = () => {
           {post && (post.content.length / 1000).toFixed(0)}mins read
         </span>
       </div>
-      <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{ __html: post?.content }}></div>
+      <div
+        className="p-3 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post?.content }}
+      ></div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
+      </div>
     </main>
   );
 };
